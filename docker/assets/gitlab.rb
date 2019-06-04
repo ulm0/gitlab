88a873cfb786886b854b037df2fb3232878ff2ef
Pierre Ugaz <ulm0@innersea.xyz>
2019-06-04 11:23:19 -0400

    update changes
    Signed-off-by: Pierre Ugaz <ulm0@innersea.xyz>

--- docker/assets/gitlab.rb
+++ docker/assets/gitlab.rb
@@ -30,8 +30,13 @@ nginx['worker_connections'] = 2048
 host = `hostname`.strip
 external_url "http://#{host}"
 
+# Explicitly disable init detection since we are running on a container
+package['detect_init'] = false
+
 # Load custom config from environment variable: GITLAB_OMNIBUS_CONFIG
-eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s
+# Disabling the cop since rubocop considers using eval to be security risk but
+# we don't have an easy way out, atleast yet.
+eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s # rubocop:disable Security/Eval
 
 # Load configuration stored in /etc/gitlab/gitlab.rb
 from_file("/etc/gitlab/gitlab.rb")
