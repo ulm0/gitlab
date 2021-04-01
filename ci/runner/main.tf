ffa4a1fcb07ccdd325f6aed35277bf7f1c618cb5
Pierre Ugaz <ulm0@innersea.xyz>
2021-04-01 18:57:49 -0300

    chore: bump 13.10.0

--- ci/runner/main.tf
+++ ci/runner/main.tf
@@ -1,25 +1,18 @@
 terraform {
   backend "s3" {
-    bucket                      = "ulm0"
-    key                         = "gitlab/gitlab.tfstate"
-    skip_credentials_validation = true
-    skip_region_validation      = true
+    bucket  = "ulm0-tf-backend"
+    key     = "runner/gitlab-arm"
+    encrypt = true
+    region  = "us-east-1"
   }
 }
 
-provider "scaleway" {
-  region = "fr-par"
-  zone   = "fr-par-1"
+provider "aws" {}
+
+module "arm" {
+  source          = "git::https://gitlab.com/innersea/tf-modules/gitlab-runner.git"
+  gitlab_token    = var.gitlab_token
+  run_as_platform = "arm"
 }
 
-module "runner" {
-  source                 = "git::https://gitlab.com/innersea/tf-modules/gitlab-runner.git"
-  arch                   = "arm"
-  bootscript_name_filter = "4.9 latest"
-  gitlab_site            = var.gitlab_site
-  gitlab_token           = var.gitlab_token
-  server_type            = "C1"
-}
-
-variable "gitlab_site" {}
 variable "gitlab_token" {}
